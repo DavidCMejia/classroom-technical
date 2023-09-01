d3f648995075bf6109b69300fb75a742da6fa05a
David <dcmejiaz@geosat.com.co>
2023-08-31 20:23:17 -0500

    creacion admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'role', 'email', 'password')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email')
+    list_per_page = 25
--- users/models/user.py
+++ users/models/user.py
@@ -3,6 +3,23 @@ from django.db import models
 
 class User(AbstractUser):
     name = models.CharField(max_length=100)
-    role = models.CharField(max_length=100)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.name
+    
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'role'
+        verbose_name_plural = 'roles'
+
+    def __str__(self):
+        return self.name
